8439363055f558a8b4ebeb8f49f0e49abb4e9317
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2021-12-31 20:43:30 +0200

    activate the toggle button by dispatch from redux store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,8 +5,11 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   const incrementHandler = () => {
     dispatch({ type: "increment", amount: 1 });
@@ -22,7 +25,7 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={incrementByfiveHandler}>Increase by 5</button>
--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,28 @@ import { createStore } from "redux";
 // In Backend (Node.js) calling redux is like:
 // const redux = require("redux");
 
-const counterReducer = (state = { counter: 0, amount: 1 }, action) => {
+const initialState = { counter: 0, amount: 1, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + action.amount,
+      amount: state.amount,
+      showCounter: state.showCounter,
     };
   }
   if (action.type === "decrement") {
     return {
       counter: state.counter - action.amount,
+      amount: state.amount,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "toggle") {
+    return {
+      counter: state.counter,
+      amount: state.amount,
+      showCounter: !state.showCounter,
     };
   }
   return state;
